80792bedd68fb169df6f4749f79c710b4a9d7a19
BryanBattershill <bryan.battershill@gmail.com>
2019-11-20 21:15:23 -0500

    Initial discord bot setup

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-var Discord = require('discord.io');
-var auth = require('./auth.json');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
+
+client.login("NjQ1MzIyODMyNjMyMzQ4NzAy.XdIpAw.SjfJE6GY7qnb0Gw6lKhB0T8Sp4I");
