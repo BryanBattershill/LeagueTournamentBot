3e98c37a2c91fc7f9bcef26b7a6cb7b29ec21b9e
BryanBattershill <bryan.battershill@gmail.com>
2019-11-21 14:08:36 -0500

    Bot waits N seconds for reactions

--- index.js
+++ index.js
@@ -6,9 +6,32 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
+  var startCommand = '!start'
+  if (msg.content.startsWith === startCommand && (msg.member.roles.find(r => r.name === "Admin")) || msg.author.id === msg.guild.ownerID) {
+    content = ("Welcome to all members of the UOttawa League Club.\n"+
+    "This marks the start of the weekly custom game next sunday.\n"+
+    "If you would like to join, react with 👍.")
+    var args = msg.content.slice(startCommand.length).split(' ').slice(1);
+    console.log(args)
+    if (args.length===0){
+      console.log('No args')
+      return 
+    }
+    msg.channel.send(content).then(sentMessage =>{
+      sentMessage.react('👍')
+      const filter = (reaction) => reaction.emoji.name === '👍'
+      var time = args[args.length-1].split(':')
+      args = args.slice(0,3)
+      args[1] = parseInt(args[1]-1,10).toString()
+      args.push(...time)
+      tourneyStart = new Date(...args)
+      console.log(tourneyStart)
+      console.log(tourneyStart-Date.now())
+      sentMessage.awaitReactions(filter, { time: tourneyStart-Date.now() })
+        .then(collected => {console.log(`Collected ${collected.size} reactions`)})
+        .catch(console.error);
+    })
   }
 });
 
-client.login("NjQ1MzIyODMyNjMyMzQ4NzAy.XdIpAw.SjfJE6GY7qnb0Gw6lKhB0T8Sp4I");
+client.login("NjQ1MzIyODMyNjMyMzQ4NzAy.XdatxQ.M8cweitqCnKP9KSXGEqKcavliLc");
