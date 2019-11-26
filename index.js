1801473e642f524e6d5d8831db58849faa0c5b6c
BryanBattershill <bryan.battershill@gmail.com>
2019-11-26 02:06:30 -0500

    New user model. Get reacted users, add to array

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
+const {User} = require('./src/Models')
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
-
 client.on('message', msg => {
   var startCommand = '!start'
   if (msg.content.startsWith === startCommand && (msg.member.roles.find(r => r.name === "Admin")) || msg.author.id === msg.guild.ownerID) {
@@ -15,23 +15,37 @@ client.on('message', msg => {
     console.log(args)
     if (args.length===0){
       console.log('No args')
-      return 
+      //return
     }
+    var userArr = []
     msg.channel.send(content).then(sentMessage =>{
       sentMessage.react('👍')
       const filter = (reaction) => reaction.emoji.name === '👍'
-      var time = args[args.length-1].split(':')
-      args = args.slice(0,3)
-      args[1] = parseInt(args[1]-1,10).toString()
-      args.push(...time)
-      tourneyStart = new Date(...args)
-      console.log(tourneyStart)
-      console.log(tourneyStart-Date.now())
+      // var time = args[args.length-1].split(':')
+      // args = args.slice(0,3)
+      // args[1] = parseInt(args[1]-1,10).toString()
+      // args.push(...time)
+      // tourneyStart = new Date(...args)
+      // console.log(tourneyStart)
+      // console.log(tourneyStart-Date.now())
+      var tourneyStart = Date.now()+5000
       sentMessage.awaitReactions(filter, { time: tourneyStart-Date.now() })
-        .then(collected => {console.log(`Collected ${collected.size} reactions`)})
+        .then(collected => {
+          console.log(`Collected ${collected.size} reactions`)
+          var temp = collected.get('👍')
+          // console.log(temp.users)
+          for (const user of temp.users){
+            if (user[0] == client.user.id){
+              continue
+            }
+            var temp = new User(user[1].id, user[1].username, user[1].discriminator)
+            userArr.push(temp)
+          }
+          console.log(userArr)
+      })
         .catch(console.error);
     })
   }
 });
 
-client.login("NjQ1MzIyODMyNjMyMzQ4NzAy.XdatxQ.M8cweitqCnKP9KSXGEqKcavliLc");
+client.login("NjQ1MzIyODMyNjMyMzQ4NzAy.XdbmnA.1LIQ2zaWiD47I_BH_PyEXFvCO3A");
---
+++ src/Models/User.js
@@ -0,0 +1,9 @@
+class User{
+    constructor(id, name, disc){
+        this.id = id
+        this.name = name
+        this.disc = disc
+    }
+}
+
+module.exports = User
